41140ab2bb78b22e3201c022b5316b6bb36dd0b3
Hemant Kumar <hmntkmr285@gmail.com>
2023-05-16 18:48:57 +0530

    Initial Commit

---
+++ CurveTabBar/CurveTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurveTabBarApp.swift
+//  CurveTabBar
+//
+//  Created by Hemant kumar on 16/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CurveTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
